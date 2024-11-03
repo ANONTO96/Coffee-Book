4acafc0270e42993304f114f2b80be0c25b2930c
ANONTO <tanjirulanonto04@gmail.com>
2024-11-03 23:02:37 +0600

    some design sorted

--- src/pages/CoffeeDetails.jsx
+++ src/pages/CoffeeDetails.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useLoaderData, useParams } from "react-router-dom";
+import img from '../assets/nutrition.png'
 
 const CoffeeDetails = () => {
     const data = useLoaderData();
@@ -13,7 +14,7 @@ const CoffeeDetails = () => {
     return (
         <div className="space-y-1 lg:space-y-3">
             <h1 className="text-xl md:text-2xl lg:text-3xl font-thin">{coffee.description}</h1>
-            <img className="rounded-2xl w-[1000px] lg:h-[600px] object-contain " src={coffee.image} alt="coffee" />
+            <img className="rounded-2xl lg:h-[550px] object-contain " src={coffee.image} alt="coffee" />
             <h1 className="text-2xl md:text-3xl lg:text-4xl font-thin">{coffee.name}</h1>
             <br />
             <p className="text-gray-600">Popularity: {coffee.popularity}</p>
@@ -27,21 +28,26 @@ const CoffeeDetails = () => {
             <br />
             <h1 className="text-2xl md:text-3xl lg:text-4xl font-thin">Ingredients :</h1>
             <ul className="text-gray-600">
-                {/* {coffee?.ingredients?.[0] && <li>{coffee.ingredients[0]}</li>}
-                {coffee?.ingredients?.[1] && <li>{coffee.ingredients[1]}</li>} */}
-                {coffee.ingredients?.map((ingredient, index) => (
-                    <li key={index}>{ingredient}</li>
-                ))}
+                {
+                    coffee.ingredients?.map((ingredient, index) => (
+                        <li key={index}>{ingredient}</li>
+                    ))}
             </ul>
-            <br />
-            <h1 className="text-2xl md:text-3xl lg:text-4xl font-thin">Nutrition :</h1>
-            <ul className="text-gray-600">
-                <li>1. Calories: {coffee?.nutrition_info?.calories}</li>
-                <li>2. Fat: {coffee?.nutrition_info?.fat}</li>
-                <li>3. Carbohydrates: {coffee?.nutrition_info?.carbohydrates}</li>
-                <li>4. Protein: {coffee?.nutrition_info?.protein}</li>
+            <div className="flex justify-between items-center lg:w-[80%]">
+                <div>
+                    <h1 className="text-2xl md:text-3xl lg:text-4xl font-thin">Nutrition :</h1>
+                    <ul className="text-gray-600">
+                        <li>1. Calories: {coffee?.nutrition_info?.calories}</li>
+                        <li>2. Fat: {coffee?.nutrition_info?.fat}</li>
+                        <li>3. Carbohydrates: {coffee?.nutrition_info?.carbohydrates}</li>
+                        <li>4. Protein: {coffee?.nutrition_info?.protein}</li>
 
-            </ul>
+                    </ul>
+                </div>
+                <div>
+                    <img className="w-80 sm:w-96 md:w-[450px] lg:w-[650px]" src={img} alt="" />
+                </div>
+            </div>
         </div>
     );
 };
